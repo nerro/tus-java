391a9dff71bca2b5b1b4a50d031b7baa6c66efa6
Pavel Sorokin <pavel.sorokin@nerro.eu>
2016-09-06 13:48:53 +0200

    Initialize upload store mechanism when starting tus-java-server

--- subprojects/tus-java-server/src/main/java/eu/nerro/tus/server/CliMain.java
+++ subprojects/tus-java-server/src/main/java/eu/nerro/tus/server/CliMain.java
@@ -7,10 +7,15 @@ import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 
+import eu.nerro.tus.server.configuration.ConfigVar;
+import eu.nerro.tus.server.configuration.ConfigVarHelper;
 import eu.nerro.tus.server.exception.TusExceptionHandler;
 import eu.nerro.tus.server.service.Service;
 import eu.nerro.tus.server.service.ShutdownHookService;
 import eu.nerro.tus.server.service.http.HttpUploadService;
+import eu.nerro.tus.server.store.Datastore;
+import eu.nerro.tus.server.store.Store;
+import eu.nerro.tus.server.store.Stores;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -25,6 +30,18 @@ public class CliMain {
     // setup exception handler to handle all uncaught (runtime) exceptions
     Thread.setDefaultUncaughtExceptionHandler(new TusExceptionHandler());
 
+    // initialize upload store mechanism
+    final Store store;
+    final Datastore datastore = getConfiguredDatastore();
+    switch (datastore) {
+      case FILESTORE:
+        store = Stores.newFileStore();
+        break;
+
+      default:
+        throw new IllegalArgumentException("No valid datastore defined: '" + datastore + "'");
+    }
+
     final Service httpUploadService = new HttpUploadService();
     ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     scheduler.execute(httpUploadService);
@@ -33,5 +50,28 @@ public class CliMain {
 
     final long endTime = System.nanoTime();
     LOG.info("TUS Java server started in {} ms", TimeUnit.MILLISECONDS.convert(endTime - startTime, TimeUnit.NANOSECONDS));
+  }
+
+  /**
+   * Reads the config {@link ConfigVar#DATASTORE} from environment variables.
+   * <p>
+   * <i>Note:</i> if the variable is not defined or could not be parsed,
+   * then {@link Datastore#FILESTORE} will be returned as default value.
+   */
+  private static Datastore getConfiguredDatastore() {
+    final String datastoreConfigVar = ConfigVarHelper.getEnvValue(ConfigVar.DATASTORE);
+    if (datastoreConfigVar == null || datastoreConfigVar.isEmpty()) {
+      LOG.warn("Configuration variable '{}' is not defined, use '{}' as default value", ConfigVar.DATASTORE, Datastore.FILESTORE);
+      return Datastore.FILESTORE;
+    }
+
+    Datastore datastore = Datastore.fromValue(datastoreConfigVar);
+
+    if (Datastore.UNKNOWN.equals(datastore)) {
+      LOG.warn("Environment variable '{}={}' could not be parsed, use '{}'", ConfigVar.DATASTORE, datastoreConfigVar, Datastore.FILESTORE);
+      datastore = Datastore.FILESTORE;
+    }
+
+    return datastore;
   }
 }
